673aefe8288274b38541bce764ff83676269440d
zunda <zunda at freehsell.org>
2014-05-26 10:45:06 -1000

    Read raw ADC values

---
+++ ReadAdc/ReadAdc.ino
@@ -0,0 +1,26 @@
+/*
+ ReadAdc.ino - read ADCs
+
+ Based upon an example code ReadAnalogVoltage in the public domain
+ that comes with the Arduino IDE.
+
+ Copyright (C) 2014 zunda <zunda at freeshell.org>
+
+ Permission is granted for use, copying, modification,
+ distribution, and distribution of modified versions of this work
+ as long as the above copyright notice is included.
+ */
+
+const int pin1 = A0;
+const int pin2 = A1;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+	Serial.print(analogRead(pin1));
+	Serial.print("\t");
+	Serial.println(analogRead(pin2));
+  delay(1000);
+}
