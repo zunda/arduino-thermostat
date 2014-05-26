b9276044dd309bc198024648d523c2c68b485594
zunda <zunda at freehsell.org>
2014-05-26 11:40:39 -1000

    Auto-format

--- ReadAdc/ReadAdc.ino
+++ ReadAdc/ReadAdc.ino
@@ -1,11 +1,11 @@
 /*
  ReadAdc.ino - read ADCs
-
+ 
  Based upon an example code ReadAnalogVoltage in the public domain
  that comes with the Arduino IDE.
-
+ 
  Copyright (C) 2014 zunda <zunda at freeshell.org>
-
+ 
  Permission is granted for use, copying, modification,
  distribution, and distribution of modified versions of this work
  as long as the above copyright notice is included.
@@ -19,8 +19,9 @@ void setup() {
 }
 
 void loop() {
-	Serial.print(analogRead(pin1));
-	Serial.print("\t");
-	Serial.println(analogRead(pin2));
-  delay(1000);
+  Serial.print(analogRead(pin1));
+  Serial.print("\t");
+  Serial.println(analogRead(pin2));
+  delay(60000);
 }
+
